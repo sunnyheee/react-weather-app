afabff4b91dd1468c81fda0584846fdd4e574cd3
sunnyheee <fool0116@naver.com>
2024-03-24 21:45:34 +0900

    fix: bug

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ function App() {
   useEffect(() => {
     getCurrentLocationWeather();
     getWeatherByCity(selectedCity);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedCity]);
 
   return (
